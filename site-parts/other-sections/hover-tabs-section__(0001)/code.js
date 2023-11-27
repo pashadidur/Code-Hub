0dd303fe20dde960cb81ecf9c1446eeade4dedfb
pashadidur <46746123+pashadidur@users.noreply.github.com>
2023-11-28 00:16:01 +0200

    Update code.js

--- site-parts/other-sections/hover-tabs-section__(0001)/code.js
+++ site-parts/other-sections/hover-tabs-section__(0001)/code.js
@@ -1,1 +1,8 @@
+/* HEADER SECTION */
+<link rel="stylesheet" href="https://www.ghostplugins.dev/assets/freya/freya.css">
+/* HEADER SECTION */
 
+/* FOOTER SECTION */
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://www.ghostplugins.dev/assets/freya/freya-7-1.js"></script>
+/* FOOTER SECTION */
